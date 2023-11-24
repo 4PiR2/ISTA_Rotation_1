513d0f18c58044b6b0724c2126d28efb20466be8
4PiR2 <19995777+4PiR2@users.noreply.github.com>
2023-11-24 11:48:19 +0000

    update

--- flwr_test/launcher.sh
+++ flwr_test/launcher.sh
@@ -7,26 +7,24 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=1G
-
-# number of GPUs per node
-#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node 1
 
 #Maximum runtime is limited to 10 days, ie. 240 hours
-#SBATCH --time=00:10:00
+#SBATCH --time=05:59:59
 
 #Define the GPU architecture (GTX980 in the example, other options are GTX1080Ti, K40)
-###SBATCH --constraint=GTX1080Ti
+###SBATCH --constraint=RTX2080Ti
 
 #SBATCH --job-name=Flower
 
-mkdir -p outputs/slurm
-#SBATCH --output=outputs/slurm/out.txt
+#SBATCH --output=outputs/slurm/slurm-%A_%a.out
+
+bash_args="$@"
 
 srun --cpu_bind=verbose bash -c "
 python3 launcher.py \\
 --args \"slurm_job_nodelist=[$(scontrol show hostnames $SLURM_JOB_NODELIST)]\" \\
 --num-step-clients 5 \\
---num-rounds 1000 \\
---eval-interval 0 \\
-&> outputs/slurm/out_\$(hostname).txt
+$bash_args \\
+&> outputs/slurm/slurm-$SLURM_JOBID-\$(hostname).out
 "
