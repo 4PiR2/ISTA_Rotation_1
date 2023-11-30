5690535c1a0a78ca1f77d65a0283a62cdb4c640f
4PiR2 <19995777+4PiR2@users.noreply.github.com>
2023-11-30 20:10:32 +0000

    test

--- flwr_test/models.py
+++ flwr_test/models.py
@@ -25,49 +25,24 @@ class CNNEmbed2(nn.Module):
             nn.Linear(84, embed_dim),  # (B, E)
         ]
 
-        layers2 = [
-            nn.Conv2d(in_channels=in_channels + embed_y * dim_y, out_channels=n_kernels, kernel_size=3, stride=2, padding=1),  # (B, C, 16, 16)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=n_kernels, out_channels=2 * n_kernels, kernel_size=3, stride=2, padding=1),  # (B, 2C, 8, 8)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=2 * n_kernels, out_channels=4 * n_kernels, kernel_size=3, stride=2, padding=1),  # (B, 4C, 4, 4)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=4 * n_kernels, out_channels=8 * n_kernels, kernel_size=3, stride=2, padding=1),  # (B, 8C, 2, 2)
-            nn.ReLU(inplace=True),
-            nn.Flatten(start_dim=-3, end_dim=-1),  # (B, 32C)
-            nn.Linear(32 * n_kernels, 16 * n_kernels),  # (B, 16C)
-            nn.ReLU(inplace=True),
-            nn.Linear(16 * n_kernels, 8 * n_kernels),  # (B, 8C)
-            nn.ReLU(inplace=True),
-            nn.Linear(8 * n_kernels, embed_dim),  # (B, E)
-        ]
+        # layers2 = [
+        #     nn.Conv2d(in_channels=in_channels + embed_y * dim_y, out_channels=n_kernels, kernel_size=3, stride=2, padding=1),  # (B, C, 16, 16)
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(in_channels=n_kernels, out_channels=2 * n_kernels, kernel_size=3, stride=2, padding=1),  # (B, 2C, 8, 8)
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(in_channels=2 * n_kernels, out_channels=4 * n_kernels, kernel_size=3, stride=2, padding=1),  # (B, 4C, 4, 4)
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(in_channels=4 * n_kernels, out_channels=8 * n_kernels, kernel_size=3, stride=2, padding=1),  # (B, 8C, 2, 2)
+        #     nn.ReLU(inplace=True),
+        #     nn.Flatten(start_dim=-3, end_dim=-1),  # (B, 32C)
+        #     nn.Linear(32 * n_kernels, 16 * n_kernels),  # (B, 16C)
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(16 * n_kernels, 8 * n_kernels),  # (B, 8C)
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(8 * n_kernels, embed_dim),  # (B, E)
+        # ]
 
-        layers3 = [
-            nn.Conv2d(in_channels=in_channels + embed_y * dim_y, out_channels=n_kernels, kernel_size=3, stride=2, padding=1),  # (B, C, 16, 16)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=n_kernels, out_channels=n_kernels, kernel_size=3, stride=1, padding=1),  # (B, C, 16, 16)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=n_kernels, out_channels=2 * n_kernels, kernel_size=3, stride=2, padding=1),  # (B, 2C, 8, 8)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=2 * n_kernels, out_channels=2 * n_kernels, kernel_size=3, stride=1, padding=1),  # (B, 2C, 8, 8)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=2 * n_kernels, out_channels=4 * n_kernels, kernel_size=3, stride=2, padding=1),  # (B, 4C, 4, 4)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=4 * n_kernels, out_channels=4 * n_kernels, kernel_size=3, stride=1, padding=1),  # (B, 4C, 4, 4)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=4 * n_kernels, out_channels=8 * n_kernels, kernel_size=3, stride=2, padding=1),  # (B, 8C, 2, 2)
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=8 * n_kernels, out_channels=8 * n_kernels, kernel_size=3, stride=1, padding=1),  # (B, 8C, 2, 2)
-            nn.ReLU(inplace=True),
-            nn.Flatten(start_dim=-3, end_dim=-1),  # (B, 32C)
-            nn.Linear(32 * n_kernels, 16 * n_kernels),  # (B, 16C)
-            nn.ReLU(inplace=True),
-            nn.Linear(16 * n_kernels, 8 * n_kernels),  # (B, 8C)
-            nn.ReLU(inplace=True),
-            nn.Linear(8 * n_kernels, embed_dim),  # (B, E)
-        ]
-
-        self.layers = nn.Sequential(*layers3)
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x: torch.Tensor | Tuple[torch.Tensor, torch.Tensor], y: Optional[torch.Tensor] = None):
         if not isinstance(x, torch.Tensor):
@@ -90,9 +65,9 @@ class Hyper(nn.Module):
         for i in range(n_hidden + 1):
             linear_layer = nn.Linear(embedding_dim if i == 0 else hidden_dim, hidden_dim)
             mlp_layers.append(spectral_norm(linear_layer) if spec_norm else linear_layer)
-            # if i != n_hidden:
-            #     mlp_layers.append(nn.ReLU(inplace=True))
-            mlp_layers.append(nn.ReLU(inplace=True))
+            if i != n_hidden:
+                mlp_layers.append(nn.ReLU(inplace=True))
+            # mlp_layers.append(nn.ReLU(inplace=True))
         self.mlp = nn.Sequential(*mlp_layers)
 
         layer_dict = {}
